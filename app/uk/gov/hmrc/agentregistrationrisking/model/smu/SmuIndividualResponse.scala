bc40cdbd59460af5e097cabb68893650283c7013
Mathew May <29102292+mghmay@users.noreply.github.com>
2026-05-12 09:52:39 +0100

    APB-11167 Align SMU viewer endpoint response (#74)
    * [APB-11100] model
    
    Signed-off-by: pav <173694350+paweldigital@users.noreply.github.com>
    
    * [APB-11100] model
    
    Signed-off-by: pav <173694350+paweldigital@users.noreply.github.com>
    
    * [WG][APB-11100] Do actions per Pav description
    
    * [WG][APB-11100] lastUpdatedAt - update when risking responce PR comments2
    
    * [APB-11100] fixes after sync from FE
    
    Signed-off-by: pav <173694350+paweldigital@users.noreply.github.com>
    
    * [APB-11100] refactored production cod
    
    Signed-off-by: pav <173694350+paweldigital@users.noreply.github.com>
    
    * APB-11167 Broken implementation of smu viewer endpoint
    
    * APB-11167 Fix SmuIndividualResponse
    
    * APB-11167 Update methods after rebasing
    
    * APB-11167 Update tests
    
    * APB-11167 Add authorisation to endpoint
    
    * APB-11167 Fix merge issues
    
    * APB-11167 Make amls presigned download url optional
    
    * APB-11167 Fix merge issues
    
    * APB-11167 Update model to reflect referenceId change
    
    * APB-11167 Clean up
    
    * APB-11167 Fix PR comments
    
    * APB-11167 Intitial test outline
    
    ---------
    
    Signed-off-by: pav <173694350+paweldigital@users.noreply.github.com>
    Co-authored-by: pav <173694350+paweldigital@users.noreply.github.com>
    Co-authored-by: wojciech.gradzki <gitwojciech@users.noreply.github.com>

---
+++ app/uk/gov/hmrc/agentregistrationrisking/model/smu/SmuIndividualResponse.scala
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentregistrationrisking.model.smu
+
+import play.api.libs.json.Json
+import play.api.libs.json.OFormat
+import uk.gov.hmrc.agentregistration.shared.*
+import uk.gov.hmrc.agentregistration.shared.AgentApplication.IsIncorporated
+import uk.gov.hmrc.agentregistration.shared.contactdetails.ApplicantName
+import uk.gov.hmrc.agentregistration.shared.individual.*
+import uk.gov.hmrc.agentregistration.shared.lists.IndividualName
+
+import java.time.LocalDate
+
+/** Represents an individual along with all relevant details and verification-related information required by the SMU (Secure Management Unit) to verify that
+  * person.
+  *
+  * This case class aggregates individual provided details and agent application details.
+  */
+final case class SmuIndividualResponse(
+  individual: SmuIndividualResponse.Individual,
+  entity: SmuIndividualResponse.Entity
+)
+
+object SmuIndividualResponse:
+
+  given format: OFormat[SmuIndividualResponse] = Json.format[SmuIndividualResponse]
+
+  def make(
+    ipd: IndividualProvidedDetails,
+    aa: AgentApplication
+  ) = SmuIndividualResponse(Individual.make(ipd), Entity.make(aa))
+
+  final case class Individual(
+    personReference: PersonReference,
+    resubmission: Boolean,
+    passedIdentityVerification: Boolean,
+    detailsProvidedByApplicant: Boolean,
+    individualName: IndividualName,
+    individualDateOfBirth: LocalDate,
+    individualNino: Option[Nino],
+    individualSaUtr: Option[SaUtr],
+    payeRefs: Option[List[PayeRef]],
+    vrns: Option[List[Vrn]],
+    telephoneNumber: TelephoneNumber,
+    emailAddress: EmailAddress
+  )
+
+  object Individual:
+
+    given format: OFormat[Individual] = Json.format[Individual]
+
+    private[SmuIndividualResponse] def make(ipd: IndividualProvidedDetails): Individual = Individual(
+      personReference = ipd.personReference,
+      // TODO update this once we have implemented resubmission flags
+      resubmission = false,
+      passedIdentityVerification = ipd.getPassedIv,
+      // TODO update this once we have details provided by applicant feature
+      detailsProvidedByApplicant = false,
+      individualName = ipd.individualName,
+      individualDateOfBirth =
+        ipd.getDateOfBirth match
+          case IndividualDateOfBirth.Provided(date) => date
+          case IndividualDateOfBirth.FromCitizensDetails(date) => date
+          case IndividualDateOfBirth.ApplicantProvided(date) => date
+      ,
+      individualNino =
+        ipd.individualNino match
+          case Some(IndividualNino.Provided(nino)) => Some(nino)
+          case Some(IndividualNino.FromAuth(nino)) => Some(nino)
+          case _ => None
+      ,
+      individualSaUtr =
+        ipd.individualSaUtr match {
+          case Some(IndividualSaUtr.Provided(saUtr)) => Some(saUtr)
+          case Some(IndividualSaUtr.FromAuth(saUtr)) => Some(saUtr)
+          case Some(IndividualSaUtr.FromCitizenDetails(saUtr)) => Some(saUtr)
+          case _ => None
+        },
+      payeRefs = ipd.payeRefs,
+      vrns = ipd.vrns,
+      telephoneNumber = ipd.getTelephoneNumber,
+      emailAddress = ipd.getEmailAddress.emailAddress
+    )
+
+  final case class Entity(
+    applicationReference: ApplicationReference,
+    resubmission: Boolean,
+    applicantName: ApplicantName,
+    businessType: BusinessType,
+    utr: Utr,
+    payeRefs: Option[List[PayeRef]],
+    vrns: Option[List[Vrn]],
+    crn: Option[Crn],
+    amlsSupervisoryBody: AmlsCode,
+    amlsRegNumber: AmlsRegistrationNumber,
+    amlsExpiryDate: Option[LocalDate],
+    amlsEvidenceReferenceId: Option[String],
+    applicantPhone: Option[TelephoneNumber],
+    applicantEmail: Option[EmailAddress]
+  )
+
+  object Entity:
+
+    given format: OFormat[Entity] = Json.format[Entity]
+
+    private[SmuIndividualResponse] def make(
+      aa: AgentApplication
+    ): Entity = Entity(
+      applicationReference = aa.applicationReference,
+      // TODO update this once we have implemented resubmission flags
+      resubmission = false,
+      applicantName = aa.getApplicantContactDetails.applicantName,
+      businessType = aa.businessType,
+      utr = aa.getUtr,
+      payeRefs = aa.payeRefs,
+      vrns = aa.vrns,
+      crn =
+        aa match
+          case aa: IsIncorporated => Some(aa.getCrn)
+          case _ => None
+      ,
+      amlsSupervisoryBody = aa.getAmlsDetails.supervisoryBody,
+      amlsRegNumber = aa.getAmlsDetails.getRegistrationNumber,
+      amlsExpiryDate = None,
+      amlsEvidenceReferenceId = aa.getAmlsDetails.amlsEvidence.map(_.fileUploadReference.value),
+      applicantPhone = aa.applicantContactDetails.map(_.getTelephoneNumber),
+      applicantEmail = aa.applicantContactDetails.map(_.getVerifiedEmail)
+    )
