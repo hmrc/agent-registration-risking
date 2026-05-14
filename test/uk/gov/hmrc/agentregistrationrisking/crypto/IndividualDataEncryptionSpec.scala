446a8f73b9fc8b199ad3255b312c9f618f641e2b
wojciech.gradzki <gitwojciech@users.noreply.github.com>
2026-05-14 19:45:49 +0100

    [APB-11332]Field Encription for Application and Individuals 4

---
+++ test/uk/gov/hmrc/agentregistrationrisking/crypto/IndividualDataEncryptionSpec.scala
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentregistrationrisking.crypto
+
+import play.api.libs.json.Json
+import uk.gov.hmrc.agentregistration.shared.Nino
+import uk.gov.hmrc.agentregistration.shared.SaUtr
+import uk.gov.hmrc.agentregistration.shared.individual.IndividualNino
+import uk.gov.hmrc.agentregistration.shared.individual.IndividualSaUtr
+import uk.gov.hmrc.agentregistration.shared.risking.submitforrisking.IndividualData
+import uk.gov.hmrc.agentregistrationrisking.testsupport.ISpec
+import uk.gov.hmrc.agentregistrationrisking.testsupport.testdata.TdRiskingInstancesInStates
+
+class IndividualDataEncryptionSpec
+extends ISpec:
+
+  override protected def configOverrides: Map[String, Any] = Map[String, Any](
+    "field-level-encryption.enabled" -> true
+  )
+
+  private val fieldLevelEncryption: FieldLevelEncryption = app.injector.instanceOf[FieldLevelEncryption]
+  private val individualDataEncryption: IndividualDataEncryption = app.injector.instanceOf[IndividualDataEncryption]
+
+  private def encrypt(plain: String): String = fieldLevelEncryption.encrypt(plain)
+
+  private val individualData: IndividualData = TdRiskingInstancesInStates.approved.individual1.individualData
+  private val encrypted: IndividualData = individualDataEncryption.encrypt(individualData)
+
+  "IndividualDataEncryption.encrypt sets ciphertext on every PII field" - {
+
+    "individualName is encrypted" in:
+      encrypted.individualName.value shouldBe encrypt(individualData.individualName.value)
+
+    "internalUserId is encrypted" in:
+      encrypted.internalUserId.value shouldBe encrypt(individualData.internalUserId.value)
+
+    "telephoneNumber is encrypted" in:
+      encrypted.telephoneNumber.value shouldBe encrypt(individualData.telephoneNumber.value)
+
+    "emailAddress is encrypted" in:
+      encrypted.emailAddress.value shouldBe encrypt(individualData.emailAddress.value)
+    
+    "vrns are encrypted element-wise" in:
+      encrypted.vrns.map(_.value) shouldBe individualData.vrns.map(v => encrypt(v.value))
+
+    "payeRefs are encrypted element-wise" in:
+      encrypted.payeRefs.map(_.value) shouldBe individualData.payeRefs.map(p => encrypt(p.value))
+  }
+
+  "IndividualDataEncryption.encrypt leaves non-PII / search-key fields untouched" - {
+
+    "personReference stays plaintext (search key)" in:
+      encrypted.personReference shouldBe individualData.personReference
+
+    "isPersonOfControl stays plaintext" in:
+      encrypted.isPersonOfControl shouldBe individualData.isPersonOfControl
+
+    "individualDateOfBirth stays plaintext (not a String-backed field)" in:
+      encrypted.individualDateOfBirth shouldBe individualData.individualDateOfBirth
+
+    "passedIv stays plaintext" in:
+      encrypted.passedIv shouldBe individualData.passedIv
+  }
+
+  "IndividualDataEncryption handles every IndividualNino / IndividualSaUtr branch" - {
+
+    "IndividualNino.NotProvided is unchanged" in:
+      individualDataEncryption.encrypt(individualData.copy(individualNino = IndividualNino.NotProvided)).individualNino shouldBe IndividualNino.NotProvided
+
+    "IndividualSaUtr.NotProvided is unchanged" in:
+      individualDataEncryption.encrypt(individualData.copy(individualSaUtr = IndividualSaUtr.NotProvided)).individualSaUtr shouldBe IndividualSaUtr.NotProvided
+    
+  }
+
+  "IndividualDataEncryption round-trips and does not leak plaintext PII" - {
+
+    "decrypt(encrypt(x)) == x" in:
+      individualDataEncryption.decrypt(individualDataEncryption.encrypt(individualData)) shouldBe individualData
+
+    "rendered JSON of the encrypted individualData contains no plaintext PII" in:
+      val rendered = Json.toJson(encrypted).toString
+      val plaintextPii: List[String] =
+        List(
+          individualData.individualName.value,
+          individualData.internalUserId.value,
+          individualData.telephoneNumber.value,
+          individualData.emailAddress.value
+        ) ++
+          (individualData.individualNino match { case IndividualNino.Provided(n) => List(n.value); case _ => Nil }) ++
+          (individualData.individualSaUtr match { case IndividualSaUtr.Provided(s) => List(s.value); case _ => Nil }) ++
+          individualData.vrns.map(_.value) ++
+          individualData.payeRefs.map(_.value)
+
+      plaintextPii.foreach: plaintext =>
+        withClue(s"plaintext '$plaintext' must not appear as a JSON value in the encrypted JSON: "):
+          rendered should not include s"\"$plaintext\""
+  }
