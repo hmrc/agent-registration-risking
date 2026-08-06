84a9aceab62f9b50ad1f32a860a055300e5e4658
Mathew May <29102292+mghmay@users.noreply.github.com>
2026-08-06 15:18:44 +0100

    APB-11696 Enable test coverage and update deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,15 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.7.0"
-  private val hmrcMongoVersion = "2.12.0"
+  private val bootstrapVersion = "10.8.0"
+  private val hmrcMongoVersion = "2.13.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % hmrcMongoVersion,
     "uk.gov.hmrc.objectstore" %% "object-store-client-play-30" % "2.6.0",
     "uk.gov.hmrc" %% "crypto-json-play-30" % "8.4.0",
-    "com.softwaremill.quicklens" %% "quicklens" % "1.9.12",
+    "com.softwaremill.quicklens" %% "quicklens" % "1.9.15",
     "org.typelevel" %% "cats-core" % "2.13.0"
   )
 
