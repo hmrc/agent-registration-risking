d633df5ed88460dd8df04ca37f20849ff098fd9a
Mathew May <29102292+mghmay@users.noreply.github.com>
2026-08-14 10:56:22 +0100

    APB-11696-2 Turn off code coverage locally

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -11,13 +11,15 @@ object CodeCoverageSettings {
     "app.*",
     "prod.*",
     ".*Routes.*",
-    ".*.testOnly.*"
+    "uk.gov.hmrc.agentregistrationrisking.testOnly.*"
   )
 
   val settings: Seq[Setting[_]] = Seq(
+//    ScoverageKeys.coverageEnabled := true, // breaks when run through jenkins see https://github.com/scoverage/sbt-scoverage/issues/84#issuecomment-263026890
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
     ScoverageKeys.coverageMinimumStmtTotal := 100,
     ScoverageKeys.coverageFailOnMinimum := false,
     ScoverageKeys.coverageHighlighting := true
   )
+
 }
